01a936980ce10108ab2d89c2fae9ffcf6ee72426
Francisco-UPX <franciscosantanaalmeida1@gmail.com>
2025-08-20 14:31:06 -0300

    Meu Controle de fluxo

---
+++ conta-banco/src/ControledeFluxo/Contador.java
@@ -0,0 +1,35 @@
+package ControledeFluxo;
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número: ");
+        int parametroUm = terminal.nextInt();
+
+        System.out.print("Digite o segundo número: ");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+
+        terminal.close();
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        // validação
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro.");
+        }
+
+        int contagem = parametroDois - parametroUm;
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
+    }
+}
+
---
+++ conta-banco/src/ControledeFluxo/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package ControledeFluxo;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
+}
